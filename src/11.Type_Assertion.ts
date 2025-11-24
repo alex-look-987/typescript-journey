7d42f2d14f82bc6460bce473f7273c065b10c332
alex-look-987 <sqrpshr.23@gmail.com>
2025-11-23 22:06:28 -0600

    feat: type assertion lesson

---
+++ src/11.Type_Assertion.ts
@@ -0,0 +1,20 @@
+
+let someValue: any = "This is a string";
+let strLength: number = (<string>someValue).length;
+// Or using as-syntax
+let strLengthAs: number = (someValue as string).length;
+
+let data: any = "Hello, TypeScript!";
+
+function getStringLength(value: any): number {
+
+    return (value as string).length;
+}
+
+console.log(getStringLength(data));
+
+
+// Example 2
+
+let phone = document.getElementById('phone') as HTMLInputElement;
+let phoneAlt = <HTMLInputElement> document.getElementById('phone');
