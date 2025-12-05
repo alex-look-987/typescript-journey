b3ee2afc23b1a006ecbd6d49213d3043b24d2ba2
Azael Vazquez <sqrpshr.23@gmail.com>
2025-12-05 14:53:02 +0000

    feat: logger

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
 import express from 'express';
 import userRoutes from './routes/userRoutes';
+import { logger } from './middleware/logger';
 import { Application, Request, Response } from 'express';
 
 const app: Application = express()
 const PORT = process.env.PORT || 3000;
+
+app.use(logger);
 
 app.use(express.json());
 
@@ -12,6 +15,7 @@ app.use('/api', userRoutes);
 app.get('/', (req: Request, res: Response) => {
     res.send('API is up and running!');
 });
+
 
 app.listen(PORT, () => {
     console.log('Server is listeing on port:', PORT) 
